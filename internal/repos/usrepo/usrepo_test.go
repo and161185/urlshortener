b172c37e01a268b6bd7435e6b69c066bf3eb4007
Andrey <and161185@gmail.com>
2022-02-08 18:29:05 +0300

    linters

--- internal/repos/usrepo/usrepo_test.go
+++ internal/repos/usrepo/usrepo_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"urlshortener/internal/models"
 
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -66,9 +67,27 @@ func TestGetStats(t *testing.T) {
 		Url: "http:\\yandex.ru",
 	}
 	su, _ := us.GenerateShortUrl(fus)
-	us.RegisterClick(su.ShortId, "127.0.0.1")
+	err := us.RegisterClick(su.ShortId, "127.0.0.1")
+	if err != nil {
+		log := getLog()
+		log.Error(err)
+	}
 	stats, _ := d.GetStats(su.StatId)
 
 	assert.Equal(t, int64(1), stats.ClickCount)
 	assert.Equal(t, "127.0.0.1", stats.Clicks[0].IP)
+}
+
+func getLog() *logrus.Logger {
+	log := logrus.New()
+	log.Level = logrus.DebugLevel
+	Formatter := new(logrus.TextFormatter)
+
+	Formatter.TimestampFormat = "2006-01-02 15:04:05"
+	Formatter.FullTimestamp = true
+	log.SetFormatter(Formatter)
+
+	log.Formatter = new(logrus.JSONFormatter)
+
+	return log
 }
